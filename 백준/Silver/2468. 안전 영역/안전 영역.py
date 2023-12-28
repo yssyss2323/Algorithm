b836021b2aafec85c0bdbc00af1a453c8a9ed5e7
심유승_T6092 <146207162+DorianYellow@users.noreply.github.com>
2023-12-28 21:24:13 +0900

    [Silver I] Title: 안전 영역, Time: 304 ms, Memory: 118468 KB -BaekjoonHub

--- 백준/Silver/2468. 안전 영역/안전 영역.py
+++ 백준/Silver/2468. 안전 영역/안전 영역.py
@@ -1,26 +1,29 @@
 import sys
-sys.setrecursionlimit(10**6)
 input = lambda: sys.stdin.readline().rstrip()
 
 n = int(input())
 field = []
-height = set()
+height = []
 for _ in range(n):
     tmp = list(map(int, input().split()))
     field.append(tmp)
-    height = height.union(set(tmp))
-#height = sorted(heights)
+    for i in tmp:
+        if i not in height:
+            height.append(i)
 
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
 # dfs 함수 : visited[x][y]에서 시작하여 주변의 k보다 큰 값을 0으로 바꾼다
 def dfs(x, y, k):
-    visited[x][y] = 0
-    for i in range(4):
-        newx, newy = x + dx[i], y + dy[i]
-        if 0 <= newx < n and 0 <= newy < n and visited[newx][newy] > k:
-            dfs(newx, newy, k)
+    stack = [(x, y)]
+    while stack:
+        x, y = stack.pop()
+        visited[x][y] = 0
+        for i in range(4):
+            newx, newy = x + dx[i], y + dy[i]
+            if 0 <= newx < n and 0 <= newy < n and visited[newx][newy] > k:
+                stack.append((newx, newy))
 
 answerlist = [1] # 1은 아무지역도 물에 안잠긴 경우를 의미
 for h in height:
