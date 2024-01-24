b6e5f5fcc2e3caacf87365e461b37dd4aea5d854
심유승_T6092 <146207162+DorianYellow@users.noreply.github.com>
2024-01-24 15:05:39 +0900

    [Silver I] Title: 쿼드트리, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.py
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.py
@@ -2,23 +2,20 @@ n = int(input())
 quad_tree = []
 for _ in range(n):
     quad_tree.append(list(input()))
-answer = ''
-
+    
 def func(row, col, num):
     check = quad_tree[row][col]
     for i in range(row, row + num):
         for j in range(col, col + num):
             if check != quad_tree[i][j]:
                 newnum = num // 2
-                answer = ('('
+                return ('('
                           + func(row, col, newnum)
                           + func(row, col + newnum, newnum)
                           + func(row + newnum, col,newnum)
                           + func(row + newnum, col + newnum, newnum)
                           + ')')
-                return answer
     else:
-        answer = check
-        return answer
+        return check
 
 print(func(0, 0, n))
