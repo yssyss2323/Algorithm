debd3ccc33eb9c3b9cf3c694085bb02ed7a5b017
심유승_T6092 <146207162+DorianYellow@users.noreply.github.com>
2024-02-25 18:02:31 +0900

    [Gold III] Title: IQ Test, Time: 44 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Gold/1111. IQ Test/IQ Test.py
@@ -0,0 +1,19 @@
+n = int(input())
+numlist = list(map(int, input().split())) + []
+if n == 1 or (n == 2 and numlist[0] != numlist[1]):
+    print('A')
+elif n == 2 and numlist[0] == numlist[1]:
+    print(numlist[0])
+else:
+    a = (numlist[2] - numlist[1]) / (numlist[1] - numlist[0]) if numlist[1] != numlist[0] else 0
+    b = numlist[1] - a * numlist[0]
+    if int(a) != a:
+        print('B')
+        exit()
+    a = int(a)
+    for i in range(n - 1):
+        if numlist[i + 1] != a * numlist[i] + b:
+            print('B')
+            break
+    else:
+        print(int(a * numlist[-1] + b))
