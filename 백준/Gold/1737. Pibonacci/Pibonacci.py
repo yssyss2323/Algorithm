253c77815a5ac57ea95108de01605ef854ebe943
심유승_T6092 <146207162+DorianYellow@users.noreply.github.com>
2024-03-30 01:31:41 +0900

    [Gold IV] Title: Pibonacci, Time: 96 ms, Memory: 39912 KB -BaekjoonHub

---
+++ 백준/Gold/1737. Pibonacci/Pibonacci.py
@@ -0,0 +1,10 @@
+from math import pi
+
+n = int(input())
+pibo = [[0] * (int(i / pi) + 1) for i in range(n + 1)]
+pibo[0][0] = 1
+for i in range(n + 1):
+    pibo[i][-1] = 1
+    for j in reversed(range(len(pibo[i]) - 1)):
+        pibo[i][j] = (pibo[i - 1][j] + pibo[i][j + 1]) % 1000000000000000000
+print(pibo[n][0])
