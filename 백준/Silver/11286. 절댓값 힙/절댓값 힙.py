ed0b252d73a5c6f4a30f50ba449ca037f5da2d1e
심유승 <146207162+DorianYellow@users.noreply.github.com>
2024-04-04 23:50:53 +0900

    [Silver I] Title: 절댓값 힙, Time: 144 ms, Memory: 35728 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -1,21 +1,18 @@
 import heapq
 import sys
 
-input = lambda:sys.stdin.readline().rstrip()
+input = lambda: sys.stdin.readline().rstrip()
 heap_abs = []
 N = int(input())
 for _ in range(N):
     tmp = int(input())
     if tmp > 0:
-        heapq.heappush(heap_abs,tmp)
+        heapq.heappush(heap_abs, tmp)
     elif tmp < 0:
         heapq.heappush(heap_abs, -tmp - 0.5)
     else:
         if heap_abs:
             tmp2 = heapq.heappop(heap_abs)
-            if tmp2 == int(tmp2):
-                print(tmp2)
-            else:
-                print(int(-tmp2 - 0.5))
+            print(tmp2 if isinstance(tmp2, int) else int(-tmp2 - 0.5))
         else:
             print(0)
