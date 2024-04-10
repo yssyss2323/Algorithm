cfd18b0dbafda67981e71c5ebf547a91288de827
심유승 <146207162+DorianYellow@users.noreply.github.com>
2024-04-10 13:38:24 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 232 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -1,26 +1,17 @@
 n = int(input())
 
-# 루트n 이하 소수 탐색
-root_n = int(n**0.5)
-check = [False] * 2 + [True] * (root_n - 1)
-prime = []
-for i in range(root_n+1):
-    if check[i]:
-        prime.append(i)
-        for j in range(i*2,root_n+1,i):
-            check[j] = False
-
 # 루트n 이하 소인수 탐색
-yes_prime = []
+yes_prime = set()
 m = n
-while prime:
-    tmp = prime.pop()
-    if m % tmp == 0:
-        while m % tmp == 0:
-            m //= tmp
-        yes_prime.append(tmp)
+i = 2
+while i ** 2 <= m:
+    if m % i:
+        i += 1
+    else:
+        m //= i
+        yes_prime.add(i)
 if m > 1:
-    yes_prime.append(m)
+    yes_prime.add(m)
 
 # n이하 서로소 개수
 for i in yes_prime:
