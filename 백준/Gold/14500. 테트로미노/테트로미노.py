0f7b1360e474bcb3ae97602dafaac081365d4b0d
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-04-14 03:43:54 +0900

    [Gold IV] Title: 테트로미노, Time: 964 ms, Memory: 36368 KB -BaekjoonHub

--- 백준/Gold/14500. 테트로미노/테트로미노.py
+++ 백준/Gold/14500. 테트로미노/테트로미노.py
@@ -1,35 +1,24 @@
 import sys
 input = lambda: sys.stdin.readline().rstrip()
 
-n, m = map(int,input().split())
+n, m = map(int, input().split())
 paper = []
 for _ in range(n):
-    row = [-float('inf')] * 2 + list(map(int,input().split())) + [-float('inf')] * 3
+    row = [-float('inf')] * 2 + list(map(int, input().split())) + [-float('inf')] * 3
     paper.append(row)
 for _ in range(3):
     paper.append([-float('inf')] * (m + 4))
 
 answer = 0
 for i in range(n):
-    for j in range(2,m+2):
-        answer = max(answer,
-                     paper[i][j]+paper[i+1][j]+paper[i+2][j]+paper[i+3][j],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 2][j] + paper[i + 2][j+1],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 2][j] + paper[i + 2][j - 1],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j+1] + paper[i + 2][j + 1],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j-1] + paper[i + 2][j - 1],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j-1] + paper[i + 1][j + 1],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j-1] + paper[i + 2][j],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j+1] + paper[i + 2][j],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j-1] + paper[i + 1][j - 2],
-                     paper[i][j] + paper[i + 1][j] + paper[i + 1][j + 1] + paper[i + 1][j + 2],
-                     paper[i][j] + paper[i][j+1] + paper[i + 1][j] + paper[i + 1][j +1],
-                     paper[i][j] + paper[i][j + 1] + paper[i + 1][j] + paper[i + 2][j],
-                     paper[i][j] + paper[i][j + 1] + paper[i + 1][j+1] + paper[i + 2][j+1],
-                     paper[i][j] + paper[i][j + 1] + paper[i + 1][j] + paper[i + 1][j-1],
-                     paper[i][j] + paper[i][j + 1] + paper[i + 1][j+1] + paper[i + 1][j + 2],
-                     paper[i][j] + paper[i][j + 1] + paper[i][j + 2] + paper[i + 1][j],
-                     paper[i][j] + paper[i][j + 1] + paper[i][j + 2] + paper[i + 1][j+2],
-                     paper[i][j] + paper[i][j + 1] + paper[i][j + 2] + paper[i + 1][j+1],
-                     paper[i][j] + paper[i][j + 1] + paper[i][j + 2] + paper[i][j+3])
+    for j in range(2, m + 2):
+        tmp11 = max(paper[i + 2][j + 1], paper[i + 2][j - 1], paper[i + 3][j])
+        tmp12 = max(paper[i + 2][j + 1], paper[i + 1][j + 2], paper[i + 2][j])
+        tmp13 = max(paper[i + 1][j - 2], paper[i + 2][j - 1], paper[i + 1][j + 1], paper[i + 2][j])
+        tmp1 = max(paper[i + 2][j] + tmp11, paper[i + 1][j + 1] + tmp12, paper[i + 1][j - 1] + tmp13)
+        tmp21 = max(paper[i + 1][j + 1], paper[i + 2][j], paper[i + 1][j - 1])
+        tmp22 = max(paper[i + 2][j + 1], paper[i + 1][j + 2])
+        tmp23 = max(paper[i + 1][j], paper[i + 1][j + 2], paper[i + 1][j + 1], paper[i][j + 3])
+        tmp2 = max(paper[i + 1][j] + tmp21, paper[i + 1][j + 1] + tmp22, paper[i][j + 2] + tmp23)
+        answer = max(answer, paper[i][j] + paper[i + 1][j] + tmp1, paper[i][j] + paper[i][j + 1] + tmp2)
 print(answer)
