c908e7cbef3def70b326153e9d91265643a992a2
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-04-17 23:50:50 +0900

    [Gold IV] Title: 오민식, Time: 320 ms, Memory: 46748 KB -BaekjoonHub

--- 백준/Gold/1630. 오민식/오민식.py
+++ 백준/Gold/1630. 오민식/오민식.py
@@ -1,12 +1,15 @@
 n = int(input())
 
-factor = []
 prime = [False] * 2 + [True] * (n - 1)
-for i in range(2, n + 1):
+for i in range(2, int(n ** 0.5) + 1):
     if prime[i]:
-        factor.append(i)
         for j in range(i * 2, n + 1, i):
             prime[j] = False
+
+factor = []
+for i in range(1, n + 1):
+    if prime[i]:
+        factor.append(i)
 for i in range(len(factor)):
     k = 1
     while factor[i] ** (k + 1) <= n:
