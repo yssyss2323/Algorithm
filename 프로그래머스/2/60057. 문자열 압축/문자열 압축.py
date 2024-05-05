4e94d39aba609fe398cc381f59ac029b853c522d
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-05-05 18:41:11 +0900

    [level 2] Title: 문자열 압축, Time: 0.04 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,17 +1,48 @@
-def solution(s):
-    min_len = len(s)
-    for i in range(1, len(s) // 2 + 1):
-        new_s = ''
-        cnt = 1
-        for j in range(0, len(s), i):
-            if s[j:j+i] == s[j+i:j+2*i]:
+# 문자열을 n개씩 토큰화시키는 함수
+def token_of_string(given_string, n):
+    num_of_token = (len(given_string) - 1) // n + 1
+    tokens = []
+    for _ in range(num_of_token):
+        tokens.append(given_string[:n])
+        given_string = given_string[n:]
+    tokens.append('0') # 0토큰은 compression_of_string 함수에서 압축문자열을 만들때 마지막 토큰까지 무사히 합치기 위한 토큰입니다
+    # 정확히 뭐라고 설명하기가 어려운데 이거 없으면 압축문자열 만들때 마지막 토큰이 안들어가는 경우가 발생합니다
+    return tokens
+
+# 문자열을 n개 단위로 압축시키는 함수
+def compression_of_string(given_string, n):
+    tokens = token_of_string(given_string, n)
+    compressed_string = ""
+    now = 0
+    while True:
+        cnt = 1 # 연속하는 동일한 토큰의 개수를 카운트하는 변수입니다
+        check = 0 # check는 반복문 탈출을 위한 변수입니다
+        for i in range(now + 1, len(tokens)):
+            check = i
+            if tokens[now] == tokens[i]:
                 cnt += 1
             else:
-                if (cnt != 1):
-                    new_s += str(cnt)
-                new_s += s[j:j+i]
-                cnt = 1
+                break
 
-        min_len = min(min_len, len(new_s))
+        # 압축 문자열을 만드는 과정입니다
+        if cnt > 1:
+            compressed_string += str(cnt) + tokens[now]
+        else:
+            compressed_string += tokens[now]
 
-    return min_len
+        if check == len(tokens) - 1:
+            break
+        now = check
+
+    return compressed_string
+
+# 정답 구하는 함수
+def solution(s):
+    if len(s) == 1:
+        answer = 1
+    else:
+        answer = float("inf")
+        for token_length in range(1, len(s) // 2 + 1):
+            tmp_string = compression_of_string(s, token_length)
+            answer = min(answer, len(tmp_string))
+    return answer
