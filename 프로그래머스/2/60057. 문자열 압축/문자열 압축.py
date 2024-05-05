f12a04ba2af5d5d9e54992ea7d65c7db5e48bda3
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-05-05 18:31:04 +0900

    [level 2] Title: 문자열 압축, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -1,45 +1,17 @@
-def token_of_string(given_string, n):
-    num_of_token = (len(given_string) - 1) // n + 1
-    tokens = []
-    for _ in range(num_of_token):
-        tokens.append(given_string[:n])
-        given_string = given_string[n:]
-    tokens.append('0')
-    return tokens
-
-def compression_of_string(given_string, n):
-    tokens = token_of_string(given_string, n)
-    compressed_string = ""
-    now = 0
-    while True:
+def solution(s):
+    min_len = len(s)
+    for i in range(1, len(s) // 2 + 1):
+        new_s = ''
         cnt = 1
-        check = 0
-        for i in range(now + 1, len(tokens)):
-            check = i
-
-            if tokens[now] == tokens[i]:
+        for j in range(0, len(s), i):
+            if s[j:j+i] == s[j+i:j+2*i]:
                 cnt += 1
             else:
-                break
+                if (cnt != 1):
+                    new_s += str(cnt)
+                new_s += s[j:j+i]
+                cnt = 1
 
-        if cnt > 1:
-            compressed_string += f"{cnt}" + f"{tokens[now]}"
-        else:
-            compressed_string += f"{tokens[now]}"
+        min_len = min(min_len, len(new_s))
 
-
-        now = check
-        if now == len(tokens) - 1:
-            break
-
-    return compressed_string
-
-def solution(s):
-    if len(s) == 1:
-        answer = 1
-    else:
-        answer = float("inf")
-        for token_length in range(1, len(s)):
-            tmp_string = compression_of_string(s, token_length)
-            answer = min(answer, len(tmp_string))
-    return answer
+    return min_len
