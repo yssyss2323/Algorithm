1f095e8056d5de576ac49a8fc90334498f4b7f11
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-05-17 23:29:36 +0900

    [Silver III] Title: N과 M (5), Time: 216 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.py
@@ -0,0 +1,18 @@
+def bt(given, arr, n, m):
+    if len(arr) == m:
+        print(*arr)
+        return
+
+    for i in range(n):
+        tmp = given[i]
+        if arr and tmp in arr:
+            continue
+        arr.append(tmp)
+        bt(given, arr, n, m)
+        arr.pop()
+
+
+N, M = map(int, input().split())
+given_arr = list(map(int, input().split()))
+given_arr.sort()
+bt(given_arr, [], N, M)
