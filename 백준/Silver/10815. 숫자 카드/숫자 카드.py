d8ebc3ddfd23d44927740b47cfae92a59b516b56
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-08 16:50:16 +0900

    [Silver V] Title: 숫자 카드, Time: 604 ms, Memory: 121536 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,23 +1,13 @@
 N = int(input())
-card_nums = list(map(int, input().split()))
-card_nums.sort()
+card_nums = set(map(int, input().split()))
 
 M = int(input())
 given_nums = list(map(int, input().split()))
 
 answer = []
 for num in given_nums:
-    start, end = 0, N - 1
-    while start <= end:
-        mid = (start + end) // 2
-        check = card_nums[mid]
-        if num == check:
-            answer.append(1)
-            break
-        elif num < check:
-            end = mid - 1
-        else:
-            start = mid + 1
+    if num in card_nums:
+        answer.append(1)
     else:
         answer.append(0)
 print(*answer)
