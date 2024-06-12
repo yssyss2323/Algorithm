6278ee59a190d3449c8fafc70cb8c33483f48a85
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-12 19:38:30 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 860 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+
+N, P = map(int, input().split())
+melody = dict()
+cnt = 0
+for _ in range(N):
+    line, pret = map(int, input().split())
+
+    melody[line] = melody.get(line, [])
+    if not melody[line]:
+        melody[line].append(pret)
+        cnt += 1
+    else:
+        while melody[line]:
+            last_pret = melody[line][-1]
+            if last_pret > pret:
+                melody[line].pop()
+                cnt += 1
+            elif last_pret < pret:
+                melody[line].append(pret)
+                cnt += 1
+                break
+            else:
+                break
+        else:
+            melody[line].append(pret)
+            cnt += 1
+
+print(cnt)
