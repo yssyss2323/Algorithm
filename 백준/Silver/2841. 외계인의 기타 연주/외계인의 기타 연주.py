5341df3a9515916726a12f363da3439321afacf6
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-12 20:02:36 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 636 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -1,29 +1,18 @@
 import sys
 input = sys.stdin.readline
 
-
 N, P = map(int, input().split())
-melody = [None] + [[] for  _ in range(6)]
+melody = [None] + [[0] for _ in range(6)]
 cnt = 0
 for _ in range(N):
     line, pret = map(int, input().split())
 
-    if not melody[line]:
+    while melody[line][-1] > pret:
+        melody[line].pop()
+        cnt += 1
+
+    if melody[line][-1] < pret:
         melody[line].append(pret)
         cnt += 1
-    else:
-        while melody[line]:
-            if melody[line][-1] > pret:
-                melody[line].pop()
-                cnt += 1
-            else:
-                break
-        if not melody[line]:
-            melody[line].append(pret)
-            cnt += 1
-        else:
-            if melody[line][-1] < pret:
-                melody[line].append(pret)
-                cnt += 1
 
 print(cnt)
