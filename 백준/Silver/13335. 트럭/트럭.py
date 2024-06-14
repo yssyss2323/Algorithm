1bf5a3239ef668259364648a72887661d23593dc
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-14 17:02:12 +0900

    [Silver I] Title: 트럭, Time: 76 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/13335. 트럭/트럭.py
+++ 백준/Silver/13335. 트럭/트럭.py
@@ -7,16 +7,18 @@ total_weight = 0
 
 time = 0
 for i in range(n):
-    truck = trucks[i]
+    truck = trucks[i] # 다리에 올라가려는 트럭
     while True:
+        # step 1 : 다리의 끝에 도달한 트럭 내보내기
         total_weight -= q[0]
         q[0] = 0
+        # step 2 : 다리상태 업데이트 및 시간 갱신
         q.rotate(-1)
         time += 1
-
+        # step 3 : 현재 트럭이 올라가도 무게제한에 안걸리면 트럭 진입
         if total_weight + truck <= L:
             q[w - 1] = truck
             total_weight += truck
             break
 
-print(time + w)
+print(time + w) # 위 로직상 마지막 트럭이 진입하면 반복문 종료 -> w를 더해줘야 함
