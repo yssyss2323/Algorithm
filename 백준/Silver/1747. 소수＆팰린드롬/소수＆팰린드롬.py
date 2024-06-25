98670ac4fd7ff0972cb1b7ac78bb7bafafecdac1
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-25 22:19:28 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 516 ms, Memory: 46788 KB -BaekjoonHub

---
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.py
@@ -0,0 +1,21 @@
+def check_pelindrom(n):
+    m = str(n)
+    for i in range(len(m) // 2):
+        if m[i] != m[len(m) - i - 1]:
+            return False
+    else:
+        return True
+
+
+MAXIMUM = 1003001  # 체로 100만 이상인 조건 충족하는 수를 찾았음
+n = int(input())
+prime = [False] * 2 + [True] * (MAXIMUM - 1)
+for i in range(2, MAXIMUM + 1):
+    if prime[i]:
+        for j in range(2 * i, MAXIMUM + 1, i):
+            prime[j] = False
+
+for i in range(n, MAXIMUM + 1):
+    if prime[i] and check_pelindrom(i):
+        print(i)
+        break
