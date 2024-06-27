e178a0d66b4316080df358f8612eb732adbb4358
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-06-27 22:34:22 +0900

    [Silver III] Title: 벌집 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/27436. 벌집 2/벌집 2.py
+++ 백준/Silver/27436. 벌집 2/벌집 2.py
@@ -1,6 +1,3 @@
-n = int(input())
-m = int(((n - 1) / 3) ** 0.5)
-if 1 + 3 * m * (m + 1) < n:
-    print(m + 2)
-else:
-    print(m + 1)
+n=int(input())
+m=int(((n-1)/3)**0.5)
+print(m+2 if n>1+3*m*(m+1) else m+1)
