d4537867b3cd0be37cbbf2943531f5ca95c093ed
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-01 23:34:02 +0900

    [Gold V] Title: 보물섬, Time: 940 ms, Memory: 34148 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.py
+++ 백준/Gold/2589. 보물섬/보물섬.py
@@ -10,21 +10,41 @@ for _ in range(l):
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-ans = 0
+tmp = 0
+tmp_visited = [[False] * w for _ in range(l)]
+candidates = []
 for i in range(l):
     for j in range(w):
         if treasure_map[i][j] == 'L':
-            tmp_visited = [[False] * w for _ in range(l)]
             tmp_visited[i][j] = True
             q = deque([(i,j,0)])
             while q:
                 now_x, now_y, distance = q.popleft()
-                if distance > ans:
-                    ans = distance
+                if distance > tmp:
+                    tmp = distance
+                    candidates.append((now_x, now_y))
                 for k in range(4):
                     next_x = now_x + dx[k]
                     next_y = now_y + dy[k]
                     if 0 <= next_x < l and 0 <= next_y < w and treasure_map[next_x][next_y] =='L' and not tmp_visited[next_x][next_y]:
                         tmp_visited[next_x][next_y] = True
                         q.append((next_x, next_y, distance + 1))
+
+ans = 0
+for i,j in candidates:
+    if treasure_map[i][j] == 'L':
+        tmp_visited = [[False] * w for _ in range(l)]
+        tmp_visited[i][j] = True
+        q = deque([(i, j, 0)])
+        while q:
+            now_x, now_y, distance = q.popleft()
+            if distance > ans:
+                ans = distance
+                candidates.append((now_x, now_y))
+            for k in range(4):
+                next_x = now_x + dx[k]
+                next_y = now_y + dy[k]
+                if 0 <= next_x < l and 0 <= next_y < w and treasure_map[next_x][next_y] == 'L' and not tmp_visited[next_x][next_y]:
+                    tmp_visited[next_x][next_y] = True
+                    q.append((next_x, next_y, distance + 1))
 print(ans)
