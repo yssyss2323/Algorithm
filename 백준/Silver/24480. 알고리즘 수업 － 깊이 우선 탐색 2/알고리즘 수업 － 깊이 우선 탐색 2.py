abec0dd7484f944eb1bef9c268b5589b5d01ee95
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-01 19:11:40 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 448 ms, Memory: 60632 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n + 1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+stack = [r]
+order = 0
+visited = [0] * (n + 1)
+while stack:
+    now = stack.pop()
+    if not visited[now]:
+        order += 1
+        visited[now] = order
+        tmp = [i for i in graph[now] if not visited[i]]
+        stack += sorted(tmp)
+print(*visited[1:], sep='\n')
