ca3cad98f13c67c610b39f9173a021249e205f31
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-03 14:20:14 +0900

    [Silver I] Title: 그림, Time: 540 ms, Memory: 45052 KB -BaekjoonHub

---
+++ 백준/Silver/1926. 그림/그림.py
@@ -0,0 +1,27 @@
+dx = [1,-1,0,0]
+dy = [0,0,1,-1]
+
+n, m = map(int, input().split())
+paper = [list(map(int, input().split())) for _ in range(n)]
+
+num_paint = 0
+biggest = 0
+for i in range(n):
+    for j in range(m):
+        if paper[i][j] == 1:
+            num_paint += 1
+            stack = [(i, j)]
+            paper[i][j] = 2
+            tmp = 1
+            while stack:
+                current = stack.pop()
+                for k in range(4):
+                    xnext, ynext = current[0] + dx[k], current[1] + dy[k]
+                    if 0 <= xnext < n and 0 <= ynext < m and paper[xnext][ynext] == 1:
+                        paper[xnext][ynext] = 2
+                        tmp += 1
+                        stack.append((xnext, ynext))
+            if tmp > biggest:
+                biggest = tmp
+print(num_paint)
+print(biggest)
