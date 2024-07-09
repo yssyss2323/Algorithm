ee683ea9a824b22a381512d533d8dbe195330e40
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-09 09:21:43 +0900

    [Silver III] Title: 파일 정리, Time: 2052 ms, Memory: 41104 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.py
@@ -0,0 +1,7 @@
+n = int(input())
+ansdic = dict()
+for _ in range(n):
+    file_name, category = input().split('.')
+    ansdic[category] = ansdic.get(category, 0) + 1
+for key in sorted(ansdic.keys()):
+    print(key, ansdic[key])
