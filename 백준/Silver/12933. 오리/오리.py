7299bfd98b45dded524f40df78afca78efb329fb
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-10 13:51:45 +0900

    [Silver II] Title: 오리, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/12933. 오리/오리.py
+++ 백준/Silver/12933. 오리/오리.py
@@ -5,27 +5,30 @@ qq, uu, aa, cc, kk = 0, 0, 0, 0, 0
 ans, save = 0, 0
 for ch in sound:
     if ch =='q':
-        if save: save-= 1; qq += 1
+        if save: save-= 1; q+= 1 #; qq += 1
         else: q += 1
     elif ch == 'u':
         if q: u += 1; q -= 1
-        elif qq: uu += 1; qq -= 1
+        # elif qq: uu += 1; qq -= 1
         else: print(-1); break
     elif ch == 'a':
         if u: a += 1; u -= 1
-        elif uu: aa += 1; uu -= 1
+        # elif uu: aa += 1; uu -= 1
         else: print(-1); break
     elif ch == 'c':
         if a: c+= 1; a -= 1
-        elif aa: cc+= 1; aa -= 1
+        # elif aa: cc+= 1; aa -= 1
         else: print(-1); break
     elif ch == 'k':
-        if c: ans += 1; save += 1; c -= 1
-        elif cc: save += 1; cc -= 1
+        if c:
+            save += 1
+            c -= 1
+            #ans += 1; save += 1; c -= 1
+        # elif cc: save += 1; cc -= 1
         else: print(-1); break
     # print(q,u,a,c,k,' ', save, ans)
 else:
-    if q or u or a or c or k or qq or uu or aa or cc or kk:
+    if q or u or a or c or k: #or qq or uu or aa or cc or kk:
         print(-1)
     else:
-        print(ans)
+        print(save)
