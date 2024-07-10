4e317fac601de2fd0bf795fcbca938b03cc47a96
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-10 14:49:17 +0900

    [Gold V] Title: A와 B 2, Time: 64 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Gold/12919. A와 B 2/A와 B 2.py
+++ 백준/Gold/12919. A와 B 2/A와 B 2.py
@@ -2,34 +2,25 @@ from collections import deque
 
 s = input()
 t = input()
-rev_t = t[::-1]
 length = len(t)
 
-q = deque([(s, False)])
+q = deque([s])
 while q:
-    curr_s, rev = q.popleft()
+    curr_s = q.popleft()
     #print(curr_s, rev)
     if len(curr_s) > length:
         print(0)
         break
     if len(curr_s) == length:
         check = ''.join(curr_s)
-        if (rev and check == rev_t) or (not rev and check == t):
+        if check == t:
             print(1)
             break
-    if rev:
-        next_s1 = 'A' + curr_s
-        if next_s1 in t or next_s1 in rev_t:
-            q.append((next_s1, True))
-        next_s2 = 'B' + curr_s
-        if next_s2 in t or next_s2 in rev_t:
-            q.append((next_s2, False))
-    else:
-        next_s1 = curr_s + 'A'
-        if next_s1 in t or next_s1 in rev_t:
-            q.append((next_s1, False))
-        next_s2 = curr_s + 'B'
-        if next_s2 in t or next_s2 in rev_t:
-            q.append((next_s2, True))
+    next_s1 = curr_s + 'A'
+    if next_s1 in t or next_s1[::-1] in t:
+        q.append(next_s1)
+    next_s2 = 'B' + curr_s[::-1]
+    if next_s2 in t or next_s2[::-1] in t:
+        q.append(next_s2)
 else:
     print(0)
