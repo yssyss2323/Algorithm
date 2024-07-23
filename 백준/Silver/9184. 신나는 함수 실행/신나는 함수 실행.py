a2dcfaaed9eee475b47c1a6294753283eebe1f8d
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-07-23 22:02:04 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 976 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.py
@@ -0,0 +1,25 @@
+memo = [0] * 8000
+
+def recur(a,b,c):
+    if a <= 0 or b <= 0 or c <= 0:
+        return 1
+
+    if a > 20 or b > 20 or c > 20:
+        return recur(20, 20, 20)
+
+    idx = (a-1)*400 + (b-1) * 20 + (c-1)
+    if not memo[idx]:
+        if a < b < c:
+            memo[idx] = recur(a, b, c - 1) + recur(a, b - 1, c - 1) - recur(a, b - 1, c)
+        else:
+            memo[idx] = recur(a - 1, b, c) + recur(a - 1, b - 1, c) + recur(a - 1, b, c - 1) - recur(a - 1, b - 1, c - 1)
+
+    return memo[idx]
+
+
+while True:
+    a, b, c = map(int, input().split())
+    if (a, b, c) == (-1, -1, -1):
+        break
+
+    print(f"w({a}, {b}, {c}) = {recur(a,b,c)}")
