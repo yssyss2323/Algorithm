acbdaec3cc9a4ce04f6535843f38739b48efe402
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-08-15 13:45:59 +0900

    [Bronze I] Title: 색종이, Time: 1664 ms, Memory: 38296 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -1,10 +1,9 @@
-board = [[-1] * 1001 for _ in range(10001)]
+board = [[-1] * 1001 for _ in range(1001)]
 n = int(input())
 for x in range(n):
     a, b, d1, d2 = map(int, input().split())
     for i in range(a, a + d1):
-        for j in range(b, b + d2):
-            board[i][j] = x
+        board[i][b:b+d2] = [x] * d2
 
 ans = [0] * n
 for row in board:
