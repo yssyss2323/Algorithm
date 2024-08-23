7fc15133151cd7b0ca6943977de05cedf2343825
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-08-23 17:22:53 +0900

    [Gold IV] Title: 타일 채우기 3, Time: 352 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.py
+++ 백준/Gold/14852. 타일 채우기 3/타일 채우기 3.py
@@ -1,9 +1,8 @@
 n = int(input())
 mod = 1000000007
-dp = [1] * (n + 1)
-dp[1] = 2
+dp = [1, 2]
 tmp = dp[0] * 2
 for i in range(2, n + 1):
-    tmp += dp[i - 1] * 2
-    dp[i] = (dp[i - 2] + tmp) % mod
-print(dp[n])
+    tmp += dp[1] * 2
+    dp = [dp[1], (dp[0] + tmp) % mod]
+print(dp[1])
