5db88f98ee713f60e7800865c0ab4f1f596e1cc9
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2024-10-08 23:38:06 +0900

    [Silver I] Title: 스타트와 링크, Time: 1048 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.py
@@ -1,41 +1,34 @@
-def score(team):
-    num = len(team)
-    ans = 0
-    for i in range(num):
-        for j in range(num):
-            ans += table[team[i]][team[j]]
-    return ans
+from itertools import combinations
 
 
-def bt(check):
-    global ans
-    if len(check) == n:
-        team1, team2 = [], []
-        for i in range(n):
-            if check[i] == 1:
-                team1.append(i)
-            else:
-                team2.append(i)
-        tmp = abs(score(team1) - score(team2))
-        # print(team1, team2, score(team1), score(team2))
-        if ans > tmp:
-            ans = tmp
-
-    else:
-        if sum(check) < n // 2:
-            check.append(1)
-            bt(check)
-            check.pop()
-        if len(check) - sum(check) < n // 2:
-            check.append(0)
-            bt(check)
-            check.pop()
-
+def calculate_team_score(team):
+    score = 0
+    for i in range(len(team)):
+        for j in range(i + 1, len(team)):
+            score += synergy[team[i]][team[j]]
+    return score
 
 
 n = int(input())
 table = [list(map(int, input().split())) for _ in range(n)]
 
-ans = float('inf')
-bt([])
-print(ans)
+synergy = [[table[i][j] + table[j][i] for j in range(n)] for i in range(n)]
+
+players = list(range(1, n))
+half_team_size = n // 2
+min_diff = float('inf')
+
+for team1_rest in combinations(players, half_team_size - 1):
+    team1 = [0] + list(team1_rest)
+    team2 = [i for i in range(n) if i not in team1]
+
+    team1_score = calculate_team_score(team1)
+    team2_score = calculate_team_score(team2)
+
+    diff = abs(team1_score - team2_score)
+    if diff < min_diff:
+        min_diff = diff
+        if min_diff == 0:
+            break
+
+print(min_diff)
