306aa617029006a8dc544900551bd736083f33be
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-01-14 17:09:35 +0900

    [Gold III] Title: 아기 상어, Time: 64 ms, Memory: 35052 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.py
+++ 백준/Gold/16236. 아기 상어/아기 상어.py
@@ -1,35 +1,29 @@
 from collections import deque
 
-dx = [1, -1, 0, 0]
-dy = [0, 0, 1, -1]
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
 
 
 def find_target(room, shark_size, shark_pos):
-    target = []
-    stand = float('inf')
-    for i in range(len(room)):
-        for j in range(len(room[0])):
-            if 0 < room[i][j] < shark_size:
-                q = deque([[shark_pos, 0]])
-                d = -1
-                visited = [[False for _ in range(len(room[0]))] for _ in range(len(room))]
-                while q:
-                    curr, dist = q.popleft()
-                    if curr == [i, j]:
-                        d = dist
-                        if stand > dist:
-                            stand = dist
-                        break
-                    for k in range(4):
-                        next_x = curr[0] + dx[k]
-                        next_y = curr[1] + dy[k]
-                        if 0 <= next_x < len(room) and 0 <= next_y < len(room[0]) and not visited[next_x][next_y]:
-                            if room[next_x][next_y] <= shark_size and dist < stand:
-                                q.append([[next_x, next_y], dist + 1])
-                                visited[next_x][next_y] = True
-                if d > -1:
-                    target.append([i, j, d])
-    return target
+    targets = []
+    q = deque([[shark_pos, 0]])
+    d = float('inf')
+    visited = [[False for _ in range(len(room[0]))] for _ in range(len(room))]
+    visited[shark_pos[0]][shark_pos[1]] = True
+    while q:
+        curr, dist = q.popleft()
+        if 0 < room[curr[0]][curr[1]] < shark_size:
+            if d >= dist:
+                d = dist
+                targets.append([curr[0], curr[1], dist])
+        for k in range(4):
+            next_x = curr[0] + dx[k]
+            next_y = curr[1] + dy[k]
+            if 0 <= next_x < len(room) and 0 <= next_y < len(room[0]) and not visited[next_x][next_y]:
+                if room[next_x][next_y] <= shark_size and d > dist:
+                    q.append([[next_x, next_y], dist + 1])
+                    visited[next_x][next_y] = True
+    return targets
 
 
 def solve():
@@ -48,6 +42,11 @@ def solve():
     time = 0
     while True:
         targets = find_target(room, shark_size[0], curr_pos)
+        # print('-' * 10, time, shark_size, target)
+        # for row in room:
+        #     print(*row)
+
+        # print(target)
         if not targets:
             print(time)
             break
