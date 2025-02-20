a09710fde1632cb886d415e3c839ec1bc46ae105
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-02-20 20:59:26 +0900

    [Silver IV] Title: 비트 우정지수, Time: 268 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/12782. 비트 우정지수/비트 우정지수.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    n, m = input().split()
+    check = {'1':0, '0':0}
+    for i in range(len(n)):
+        nn, mm = n[i], m[i]
+        if nn != mm:
+            check[nn] += 1
+    print(max(check['0'], check['1']))
