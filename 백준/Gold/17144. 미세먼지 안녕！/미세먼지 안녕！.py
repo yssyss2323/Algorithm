a71275dbb2144c544bdd8127f30cd36d3ce07372
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-02-27 16:57:02 +0900

    [Gold IV] Title: 미세먼지 안녕!, Time: 3924 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
+++ 백준/Gold/17144. 미세먼지 안녕！/미세먼지 안녕！.py
@@ -1,36 +1,67 @@
-direct = [(0, 0), (0, -1), (1, 0), (-1, 0), (0, 1)]
+direct = [(0, -1), (1, 0), (-1, 0), (0, 1)]
 
 def spread_dust(room, r, c):
     for i in range(r):
         for j in range(c):
-            for k in range(1, 5):
+            for k in range(4):
                 di = i + direct[k][0]
                 dj = j + direct[k][1]
                 cnt = 0
                 if 0 <= di < r and 0 <= dj < c and room[i][j][0] != -1 and room[di][dj][0] != -1:
-                    room[di][dj][2] += room[i][j][0] // 5
+                    room[di][dj][1] += room[i][j][0] // 5
                     cnt += 1
-                room[i][j][2] -= cnt * room[i][j][0] // 5
+                room[i][j][1] -= cnt * room[i][j][0] // 5
     for i in range(r):
         for j in range(c):
-            room[i][j][0] += room[i][j][2]
-            room[i][j][2] = 0
+            room[i][j][0] += room[i][j][1]
+            room[i][j][1] = 0
 
 def cleaning(room, r, c):
-    for i in range(r):
-        for j in range(c):
-            if room[i][j][0] != -1:
-                direction = direct[room[i][j][1]]
-                di = i + direction[0]
-                dj = j + direction[1]
-                room[di][dj][2] = room[i][j][0]
-    for i in range(r):
-        for j in range(c):
-            if room[i][j][0] == -1:
-                room[i][j][2] = 0
-            else:
-                room[i][j][0] = room[i][j][2]
-                room[i][j][2] = 0
+    for xx in range(2):
+        machine = aircon[xx]
+        for i in range(c - 1):
+            if i != machine[1]:
+                room[machine[0]][i + 1][1] = room[machine[0]][i][0]
+
+        if xx == 0:
+            for i in range(machine[0], 0, -1):
+                if machine[1] != c - 1:
+                    room[i - 1][c - 1][1] = room[i][c - 1][0]
+            for i in range(machine[0]):
+                room[i + 1][0][1] = room[i][0][0]
+        else:
+            for i in range(machine[0], r - 1):
+                if machine[1] != c - 1:
+                    room[i + 1][c - 1][1] = room[i][c - 1][0]
+            for i in range(r - 1, machine[0], -1):
+                room[i - 1][0][1] = room[i][0][0]
+
+    for i in range(1, c):
+        room[0][i - 1][1] = room[0][i][0]
+        room[r - 1][i - 1][1] = room[r - 1][i][0]
+
+    for i in range(c):
+        room[0][i][0] = room[0][i][1]
+        room[0][i][1] = 0
+
+        room[r - 1][i][0] = room[r - 1][i][1]
+        room[r - 1][i][1] = 0
+
+    for i in range(1, r - 1):
+        room[i][0][0] = room[i][0][1]
+        room[i][0][1] = 0
+
+        room[i][c - 1][0] = room[i][c - 1][1]
+        room[i][c - 1][1] = 0
+
+    for xx in range(2):
+        machine = aircon[xx]
+        for i in range(1, c - 1):
+            room[machine[0]][i][0] = room[machine[0]][i][1]
+            room[machine[0]][i][1] = 0
+
+        room[machine[0]][machine[1]] = [-1, 0]
+
 
 
 if __name__ == "__main__":
@@ -43,22 +74,8 @@ if __name__ == "__main__":
         for j in range(c):
             if row[j] == -1:
                 aircon.append((i, j))
-            tmp.append([row[j], 0, 0])
+            tmp.append([row[j], 0])
         room.append(tmp)
-    for i in range(c):
-        room[0][i][1] = 1
-        room[r - 1][i][1] = 1
-    for i in range(aircon[0][0]):
-        room[i][0][1] = 2
-    for i in range(1, aircon[0][0] + 1):
-        room[i][c - 1][1] = 3
-    for i in range(aircon[1][0] + 1, r):
-        room[i][0][1] = 3
-    for i in range(aircon[1][0], r - 1):
-        room[i][c - 1][1] = 2
-    for i in range(c - 1):
-        room[aircon[0][0]][i][1] = 4
-        room[aircon[1][0]][i][1] = 4
 
     for i in range(t):
         spread_dust(room, r, c)
