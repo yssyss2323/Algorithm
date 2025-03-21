134d278a3bcf926bd54a5429f5d81585b189f95d
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-03-22 07:56:46 +0900

    [Gold IV] Title: 스도쿠, Time: 5112 ms, Memory: 142732 KB -BaekjoonHub

--- 백준/Gold/2239. 스도쿠/스도쿠.py
+++ 백준/Gold/2239. 스도쿠/스도쿠.py
@@ -1,12 +1,12 @@
-sudoku = [list(map(int, input())) for _ in range(9)]
+board = [list(map(int, input())) for _ in range(9)]
 
 zeros = []
 for i in range(9):
     for j in range(9):
-        if sudoku[i][j] == 0:
+        if board[i][j] == 0:
             zeros.append((i, j))
 
-def sol(n, board):
+def sol(n):
     if n == len(zeros):
         return board
 
@@ -24,10 +24,10 @@ def sol(n, board):
             continue
 
         board[x][y] = i
-        result = sol(n + 1, board)
+        result = sol(n + 1)
         if result:
             return result
         board[x][y] = 0
 
-for row in sol(0, sudoku):
+for row in sol(0):
     print(*row, sep='')
