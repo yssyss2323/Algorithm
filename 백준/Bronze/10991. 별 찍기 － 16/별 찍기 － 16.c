cca9f2f01d2aae9c969869fd95dd2eff89707ae4
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-04-02 22:20:21 +0900

    [Bronze III] Title: 별 찍기 - 16, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/10991. 별 찍기 － 16/별 찍기 － 16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            printf(" ");
+        }
+        for (int j = 0; j < 2 * i + 1; j++) {
+            if (j % 2 == 0) printf("*");
+            else printf(" ");
+        }
+        printf("\n");
+    }
+    return 0;
+}
