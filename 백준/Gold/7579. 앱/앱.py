8933d12eedf8d09cb09eb50b88a4b3faf4f4d618
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-04-28 16:46:20 +0900

    [Gold III] Title: 앱, Time: 272 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.py
+++ 백준/Gold/7579. 앱/앱.py
@@ -1,30 +1,27 @@
-n, m = map(int, input().split())
-mlist = list(map(int, input().split()))
-clist = list(map(int, input().split()))
-cclist = []
+import sys
+input = sys.stdin.readline
 
-dp = [0] + [float('inf')] * 10000
-for i in range(n):
-    if clist[i] == 0:
-        m -= mlist[i]
-        cclist.append(float('inf'))
-    else:
-        cclist.append(clist[i])
+# 입력 받기
+N, M = map(int, input().split())
+memory = list(map(int, input().split()))
+cost = list(map(int, input().split()))
 
-for i in range(n):
-    c = cclist[i]
-    if c == float('inf'):
-        continue
-    for j in range(10000, 0, -1):
-        if j - c < 0:
-            continue
-        if dp[j - c] != float('inf'):
-            if dp[j] == float('inf'):
-                dp[j] = dp[j - c] + mlist[i]
-            elif dp[j] < dp[j - c] + mlist[i]:
-                dp[j] = dp[j - c] + mlist[i]
+# 가능한 최대 비용: 모든 앱을 껐을 때 비용 총합
+max_cost = sum(cost)
 
-for i in range(10001):
-    if dp[i] != float('inf') and dp[i] >= m:
+# dp[i] = i 비용을 들였을 때 확보할 수 있는 최대 메모리
+dp = [0] * (max_cost + 1)
+
+# 앱 하나씩 고려
+for i in range(N):
+    m = memory[i]
+    c = cost[i]
+    # dp 갱신은 **큰 쪽부터** 해야 해 (같은 앱을 여러 번 쓰는 걸 방지하려고)
+    for j in range(max_cost, c-1, -1):
+        dp[j] = max(dp[j], dp[j-c] + m)
+
+# 최소 비용 찾기
+for i in range(max_cost + 1):
+    if dp[i] >= M:
         print(i)
         break
