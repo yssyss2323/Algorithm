bed78f12b29dbe4bb0d6218c8a2053a8e96d5f85
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-04-30 14:26:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 2832 ms, Memory: 131816 KB -BaekjoonHub

---
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.py
@@ -0,0 +1,23 @@
+n = int(input())
+nums = list(map(int, input().split()))
+
+def bisect_left(arr, x):
+    l, r = 0, len(arr)
+    while l < r:
+        m = (l + r) // 2
+        if arr[m] < x:
+            l = m + 1
+        else:
+            r = m
+    return l
+
+# dp[i] -> 길이 i + 1인 lis의 마지막 값들 중 최소값
+dp = []
+for i in range(n):
+    curr = nums[i]
+    idx = bisect_left(dp, curr)
+    if idx == len(dp):
+        dp.append(curr)
+    else:
+        dp[idx] = curr
+print(len(dp))
