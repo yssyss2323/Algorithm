585b1586a0f1b9a80d6dfe67aa858174aa1ebd40
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-05-05 12:05:41 +0900

    [Gold III] Title: 세 용액, Time: 188 ms, Memory: 110984 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,35 +1,22 @@
 n = int(input())
-arr1 = list(map(int, input().split()))
-arr1.sort()
-arr2 = [-arr1[i] for i in range(n - 1, -1, -1)]
-
-def bisect(arr, x):
-    r, l = 0, len(arr) - 1
-    while r <= l:
-        m = (r + l) // 2
-        if arr[m] < x:
-            r = m + 1
-        elif arr[m] > x:
-            l = m - 1
-        else:
-            return m
-    return m
-
+arr = list(map(int, input().split()))
+arr.sort()
 
 val = float('inf')
 ans = []
 
-for i in range(n - 1):
-    for j in range(i):
-        x = arr1[i] + arr1[j]
-        idx = bisect(arr2, x)
-        check = [-arr2[max(0, idx - 1)], -arr2[idx], -arr2[min(n - 1, idx + 1)]]
-        for c in check:
-            if c == arr1[i] or c == arr1[j]:
-                continue
-            tmp = abs(x + c)
-            if val > tmp:
-                val = tmp
-                ans = [arr1[i], arr1[j], c]
-ans.sort()
+for i in range(n - 2):
+    l, r = i + 1, n - 1
+    while l < r:
+        if val > abs(arr[i] + arr[l] + arr[r]):
+            val = abs(arr[i] + arr[l] + arr[r])
+            ans = [arr[i], arr[l], arr[r]]
+
+        if arr[i] + arr[l] + arr[r] > 0:
+            r -= 1
+        elif arr[i] + arr[l] + arr[r] < 0:
+            l += 1
+        else:
+            break
+
 print(*ans)
