9645090821af410b8ae62aa35fad43605c35a0dd
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-06-14 12:16:33 +0900

    [Silver V] Title: 공포의 면담실, Time: 584 ms, Memory: 78912 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -1,28 +1,31 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        scanner.nextLine();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        int[] arr = new int[n];
-        int ans = 0;
+        long[] arr = new long[n];
         for (int i = 0; i < n; i++) {
-            int cnt = scanner.nextInt();
-            int tmp = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int cnt = Integer.parseInt(st.nextToken());
+            long sum = 0;
             for (int j = 0; j < cnt; j++) {
-                tmp += scanner.nextInt();
+                sum += Long.parseLong(st.nextToken());
             }
-            arr[i] = tmp;
+            arr[i] = sum;
         }
+
         Arrays.sort(arr);
-        int tmp = 0;
+
+        long ans = 0;
+        long tmp = 0;
         for (int i = 0; i < n; i++) {
             tmp += arr[i];
             ans += tmp;
         }
+
         System.out.println(ans);
     }
 }
