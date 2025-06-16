947753541b5cd136528be0f17a56c82df2802f84
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-06-16 21:28:52 +0900

    [Gold V] Title: 월요일-토요일, Time: 780 ms, Memory: 191388 KB -BaekjoonHub

--- 백준/Gold/4969. 월요일－토요일/월요일－토요일.java
+++ 백준/Gold/4969. 월요일－토요일/월요일－토요일.java
@@ -1,29 +1,38 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         while (true) {
-            int num = input.nextInt();
+            String line = br.readLine();
+            if (line == null || line.isEmpty()) continue;
+
+            int num = Integer.parseInt(line);
             if (num == 1) break;
-            else {
-                System.out.print(num + ":");
-                printFactor(num);
-            }
+
+            sb.append(num).append(":");
+            sb.append(printFactor(num)).append("\n");
         }
+
+        System.out.print(sb);
     }
 
-    static void printFactor(int num) {
+    static String printFactor(int num) {
         boolean[] check = new boolean[num + 1];
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 2; i <= num; i++) {
-            if (num % i == 0 && check[i] == false && (i % 7 == 1 || i % 7 == 6)) {
-                System.out.print(" " + i);
+            if (num % i == 0 && !check[i] && (i % 7 == 1 || i % 7 == 6)) {
+                sb.append(" ").append(i);
                 for (int j = i * 2; j <= num; j += i) {
                     check[j] = true;
                 }
             }
         }
-        System.out.println();
+
+        return sb.toString();
     }
 }
