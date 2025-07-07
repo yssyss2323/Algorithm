2a94e8bcec35249587cd56d6be9c676af8a959d9
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-07-07 17:54:15 +0900

    [Gold IV] Title: 인접한 비트의 개수, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Gold/2698. 인접한 비트의 개수/인접한 비트의 개수.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+maximum = 100
+
+dp = [[[0, 0] for _ in range(maximum + 1)] for _ in range(maximum + 1)]
+dp[2][0][0] = 2
+dp[2][0][1] = 1
+dp[2][1][0] = 0
+dp[2][1][1] = 1
+for n in range(3, maximum + 1):
+    for k in range(n):
+        if k == 0:
+            dp[n][k][0] = dp[n - 1][0][0] + dp[n - 1][0][1]
+            dp[n][k][1] = dp[n - 1][0][0]
+        else:
+            dp[n][k][0] = dp[n - 1][k][0] + dp[n - 1][k][1]
+            dp[n][k][1] = dp[n - 1][k][0] + dp[n - 1][k - 1][1]
+
+for _ in range(int(input())):
+    n, k = map(int, input().split())
+    print(dp[n][k][0] + dp[n][k][1])
