cd1b895a2e4baeb093e3be5c3f16240dee1d7df8
YS_Shim <146207162+DorianYellow@users.noreply.github.com>
2025-07-15 17:59:09 +0900

    [Gold IV] Title: 수들의 합 4, Time: 116 ms, Memory: 47600 KB -BaekjoonHub

--- 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
+++ 백준/Gold/2015. 수들의 합 4/수들의 합 4.py
@@ -1,23 +1,12 @@
 n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
-tmp = 0
-prefix_dic = {0 : 1}
-prefix_set = {0}
-prefix_arr = [0]
-for a in arr:
-    tmp += a
-    prefix_dic[tmp] = prefix_dic.get(tmp, 0) + 1
-    prefix_arr.append(tmp)
-    prefix_set.add(tmp)
-
 ans = 0
-for elem in prefix_arr:
-    if prefix_dic[elem] > 1:
-        prefix_dic[elem] -= 1
-    else:
-        prefix_dic[elem] = 0
-        prefix_set.remove(elem)
-    if elem + k in prefix_set:
-        ans += prefix_dic[elem + k]
+current_sum = 0
+prefix_counts = {0: 1}
+
+for num in arr:
+    current_sum += num
+    ans += prefix_counts.get(current_sum - k, 0)
+    prefix_counts[current_sum] = prefix_counts.get(current_sum, 0) + 1
 print(ans)
