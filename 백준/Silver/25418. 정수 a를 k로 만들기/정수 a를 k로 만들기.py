57a40fa8b34307ee86a97cb26804c22804dd0362
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2025-08-13 13:33:02 +0900

    [Silver III] Title: 정수 a를 k로 만들기, Time: 988 ms, Memory: 172576 KB -BaekjoonHub

---
+++ 백준/Silver/25418. 정수 a를 k로 만들기/정수 a를 k로 만들기.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+a, k = map(int, input().split())
+check = {a}
+q = deque([(a, 0)])
+
+while True:
+    curr, cnt = q.popleft()
+    if curr > 1000000:
+        continue
+    if curr + 1 == k or curr * 2 == k:
+        print(cnt + 1)
+        break
+    if curr + 1 not in check:
+        check.add(curr + 1)
+        q.append((curr + 1, cnt + 1))
+    if curr * 2 not in check:
+        check.add(curr * 2)
+        q.append((curr * 2, cnt + 1))
