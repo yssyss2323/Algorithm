9e0a2eedaa2da883ba7f5de74eaaf84a526376dc
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2025-12-19 22:47:50 +0900

    [Bronze III] Title: 얼마?, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9325. 얼마？/얼마？.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    s = int(input())
+    for _ in range(int(input())):
+        q, p = map(int, input().split())
+        s += q * p
+    print(s)
