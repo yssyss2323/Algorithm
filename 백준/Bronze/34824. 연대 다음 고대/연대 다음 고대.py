5adf5eb72efa8574e29906fd8c27a454ba59568f
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2025-12-30 20:17:02 +0900

    [Bronze IV] Title: 연대 다음 고대, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/34824. 연대 다음 고대/연대 다음 고대.py
@@ -0,0 +1,8 @@
+for _ in range(int(input())):
+    curr = input()
+    if curr == "yonsei":
+        print("Yonsei Won!")
+        break
+    elif curr == "korea":
+        print("Yonsei Lost...")
+        break
