b07f6ec18d56077ca171c727fb526c0aaad97eb9
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2026-03-15 21:55:48 +0900

    [Gold IV] Title: 직사각형과 쿼리, Time: 1252 ms, Memory: 72740 KB -BaekjoonHub

--- 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.py
+++ 백준/Gold/14846. 직사각형과 쿼리/직사각형과 쿼리.py
@@ -7,16 +7,15 @@ suff_mat = [[[0] * 11 for _ in range(n + 1)] for _ in range(n + 1)]
 
 for i in range(1, n + 1):
     for j in range(1, n + 1):
-        tmp = [0] * 11
         for k in range(11):
-            tmp[k] += suff_mat[i - 1][j][k] + suff_mat[i][j - 1][k] - suff_mat[i - 1][j - 1][k]
-        tmp[mat[i][j]] += 1
-        suff_mat[i][j] = tmp
+            suff_mat[i][j][k] = suff_mat[i - 1][j][k] + suff_mat[i][j - 1][k] - suff_mat[i - 1][j - 1][k]
+        suff_mat[i][j][mat[i][j]] += 1
 
 q = int(input())
 for _ in range(q):
     x1, y1, x2, y2 = map(int, input().split())
-    ans = [0] * 11
-    for i in range(11):
-        ans[i] = suff_mat[x2][y2][i] - suff_mat[x1 - 1][y2][i] - suff_mat[x2][y1 - 1][i] + suff_mat[x1 - 1][y1 - 1][i]
-    print(11 - ans.count(0))
+    ans = sum(
+        1 for k in range(1, 11)
+        if suff_mat[x2][y2][k] - suff_mat[x1 - 1][y2][k] - suff_mat[x2][y1 - 1][k] + suff_mat[x1 - 1][y1 - 1][k] > 0
+    )
+    print(ans)
