2ff6ddba43f10ab4861a02dfcec815531c08dc9b
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2026-03-24 16:48:26 +0900

    [Gold V] Title: 십자 찾기, Time: 868 ms, Memory: 265148 KB -BaekjoonHub

--- 백준/Gold/32715. 십자 찾기/십자 찾기.py
+++ 백준/Gold/32715. 십자 찾기/십자 찾기.py
@@ -6,22 +6,20 @@ k = int(input())
 board = [[0] * (m + 1)]
 board += [[0] + list(map(int, input().split())) for _ in range(n)]
 
-prefix_sum_row = [[0] * (m + 1)]
+prefix_sum_row = [[0] * (m + 1) for _ in range(n + 1)]
+prefix_sum_col = [[0] * (m + 1) for _ in range(n + 1)]
+
 for i in range(1, n + 1):
-    curr_row = [0]
     tmp = 0
     for j in range(1, m + 1):
         tmp += board[i][j]
-        curr_row.append(tmp)
-    prefix_sum_row.append(curr_row)
+        prefix_sum_row[i][j] = tmp
 
-prefix_sum_col = [[0] for _ in range(n + 1)]
-prefix_sum_col[0] = [0] * (m + 1)
 for i in range(1, m + 1):
     tmp = 0
     for j in range(1, n + 1):
         tmp += board[j][i]
-        prefix_sum_col[j].append(tmp)
+        prefix_sum_col[j][i] = tmp
 
 ans = 0
 for i in range(k + 1, n + 1 - k):
