bcc5690666166c836d34e3c0ec539abace49f075
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2026-04-14 16:05:17 +0900

    [Gold V] Title: 적록색약, Time: 48 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,53 +1,37 @@
-N = int(input())
-picture = []
-for _ in range(N):
-    picture.append(list(input()))
-stack1, stack2 = [], []
-visited1 = [[False for _ in range(N)] for _ in range(N)]
-visited2 = [[False for _ in range(N)] for _ in range(N)]
+n = int(input())
+picture = [list(input()) for _ in range(n)]
+
 dx = [1, -1, 0, 0]
 dy = [0, 0, 1, -1]
 
-# 적록색약 x
-def dfs1(x, y):
-    visited1[x][y] = True
-    stack1.append([x, y])
-    while stack1:
-        now = stack1.pop()
-        for i in range(4):
-            x_next = now[0] + dx[i]
-            y_next = now[1] + dy[i]
-            if 0 <= x_next < N and 0 <= y_next < N and not visited1[x_next][y_next]:
-                if picture[x][y] == picture[x_next][y_next]:
-                    stack1.append([x_next, y_next])
-                    visited1[x_next][y_next] = True
+norm_visited = [[False] * n for _ in range(n)]
+jaejun_visited = [[False] * n for _ in range(n)]
 
-# 적록색약 o
-def dfs2(x, y):
-    visited2[x][y] = True
-    stack2.append([x, y])
-    while stack2:
-        now = stack2.pop()
-        for i in range(4):
-            x_next = now[0] + dx[i]
-            y_next = now[1] + dy[i]
-            if 0 <= x_next < N and 0 <= y_next < N and not visited2[x_next][y_next]:
-                if picture[x][y] in "RG":
-                    if picture[x_next][y_next] in "RG":
-                        stack2.append([x_next, y_next])
-                        visited2[x_next][y_next] = True
+def dfs(graph, visited, is_norm=True):
+    length = len(graph)
+
+    cnt = 0
+    for i in range(length):
+        for j in range(length):
+            if not visited[i][j]:
+                if is_norm or graph[i][j] == 'B':
+                    curr_color = [graph[i][j]] # 일반탐색 또는 색맹탐색 중 파란색 탐색
                 else:
-                    if picture[x_next][y_next] == "B":
-                        stack2.append([x_next, y_next])
-                        visited2[x_next][y_next] = True
+                    curr_color = ['R', 'G'] # 색약탐색 중 빨간색 또는 초록색 탐색
+                cnt += 1
+                visited[i][j] = True
+                stack = [(i, j)]
+                while stack:
+                    curr_x, curr_y = stack.pop()
+                    for k in range(4):
+                        nx = curr_x + dx[k]
+                        ny = curr_y + dy[k]
+                        if 0 <= nx < length and 0 <= ny < length and not visited[nx][ny]:
+                            if graph[nx][ny] in curr_color:
+                                stack.append((nx, ny))
+                                visited[nx][ny] = True
+    return cnt
 
-answer1, answer2 = 0, 0
-for i in range(N):
-    for j in range(N):
-        if not visited1[i][j]:
-            dfs1(i, j)
-            answer1 += 1
-        if not visited2[i][j]:
-            dfs2(i, j)
-            answer2 += 1
-print(answer1, answer2)
+norm_cnt = dfs(picture, norm_visited)
+jaejun_cnt = dfs(picture, jaejun_visited, False)
+print(norm_cnt, jaejun_cnt)
