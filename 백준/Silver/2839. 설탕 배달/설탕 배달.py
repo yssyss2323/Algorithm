3178eef0472d07bccab4e39f1bbcc549cde1c587
YS_Shim <146207162+yssyss2323@users.noreply.github.com>
2026-04-15 10:13:47 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,9 +1,15 @@
 n = int(input())
-if n in [1, 2, 4, 7]:
+
+if n in (1, 2, 4, 7):
     print(-1)
 else:
-    for i in range(n // 5, -1, -1):
-        if (n - 5 * i) % 3 == 0:
-            x = (n - 5 * i) // 3
-            print(i + x)
-            break
+    if n % 5 == 0:
+        print(n // 5)
+    elif n % 5 == 1:
+        print((n - 6) // 5 + 2)
+    elif n % 5 == 2:
+        print((n - 12) // 5 + 4)
+    elif n % 5 == 3:
+        print((n - 3) // 5 + 1)
+    else:
+        print((n - 9) // 5 + 3)
